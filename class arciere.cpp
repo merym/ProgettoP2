e7664d82e25c24456e04807782f38102638c5396
FilippoRuff <filipporuffin@libero.it>
2020-04-22 10:49:09 +0200

    Classi derivate dps

---
+++ class arciere.cpp
@@ -0,0 +1,21 @@
+//derivazione non virtual
+class Arciere: public DPS{
+  private:
+  	
+  public:
+  	Arciere(); //i dati come: vita, armor ecc. sono inizializzati nel costruttore che utilizza però quello della classe personaggio
+  	~Arciere();
+  	int FrecciaAppuntita(){
+  	  return 5*Critico();
+    }
+    
+    //IL fatto di impedire di usare un metodo per un tot di turni si fa con la GUI
+    
+    int FrecciaInfuocata(){
+      return 8*Critico(); 
+	}
+	
+	int FrecciaPesante(){
+	  return 7*Critico();
+	}
+};
---
+++ class soldato.cpp
@@ -0,0 +1,21 @@
+//derivazione non virtual
+class Soldato: public DPS{
+  private:
+  	
+  public:
+  	Soldato(); //i dati come: vita, armor ecc. sono inizializzati nel costruttore che utilizza però quello della classe personaggio
+  	~Soldato();
+  	int PugnoFurtivo(){
+  	  return 4*Critico();
+    }
+    
+    //IL fatto di impedire di usare un metodo per un tot di turni si fa con la GUI
+    
+    int Fendente(){
+      return 6*Critico(); 
+	}
+	
+	int Coltellata(){
+	  return 9*Critico();
+	}
+};
